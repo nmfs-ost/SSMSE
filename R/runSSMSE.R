061430243cf6a489f3428f681a098cf131da47e5
norellia-NOAA <alex.norelli@noaa.gov>
2025-09-29 14:00:27 -0400

    Fix the file transfer command to a system command

--- R/runSSMSE.R
+++ R/runSSMSE.R
@@ -942,11 +942,10 @@ run_SSMSE_iter <- function(out_dir = NULL,
                               recursive = FALSE)
       
       for (file in old_files) {
-        move_successful <- file.rename(
-          from = file, 
-          to = file.path(target_dir, basename(file))
-        )
-        if (move_successful) {
+        
+        system(paste("mv", file, file.path(target_dir, basename(file))))
+
+        if (!file.exists(file)) {
           cat("Success! The folder '", dir, "' and all its contents were moved.\n", sep="")
           cat(paste("New location:", file.path(iteration_folder, basename(dir)), "\n"))
         } else {
